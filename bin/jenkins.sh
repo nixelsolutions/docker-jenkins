da5240c1917297b310e643f5ebb813b6656a9c64
Manel Martinez Gonzalez <manel@nixelsolutions.com>
2015-09-26 19:39:50 +0200

    Start docker so we can use it inside jenkins

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -6,6 +6,7 @@ set -e
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
    exec java $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS "$@"
 fi
+exec /usr/bin/docker daemon -H unix:///var/run/docker.sock "$@"
 
 # As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
 exec "$@"
