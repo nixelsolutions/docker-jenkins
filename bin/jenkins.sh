b31367c6be50e4fe0ff8ddcbd647bf9ee520608f
Manel Martinez <manel@nixelsolutions.com>
2016-01-14 13:00:51 +0100

    Add script tools

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -5,14 +5,9 @@ set -e
 # if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
    # Install maintenance scripts
-   if [ ! -d ${SCRIPTS_PATH} ]; then
-      mkdir -p ${SCRIPTS_PATH}
-      git clone ${SCRIPTS_GIT_REPO} ${SCRIPTS_PATH}/
-   else
-      pushd ${SCRIPTS_PATH}
-      git pull
-      popd
-   fi
+   rm -rf ${SCRIPTS_PATH}
+   mkdir -p ${SCRIPTS_PATH}
+   git clone ${SCRIPTS_GIT_REPO} ${SCRIPTS_PATH}/
    for directory in `find ${SCRIPTS_PATH} -type f -name package.json -exec dirname {} \;`; do cd ${directory} && npm install; done
  
    /usr/local/bin/plugins.sh /plugins.txt
